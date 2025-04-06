4910c087197996eedc659a3d77c485c16345bbc6
“Riya <riya.shenoy.us@gmail.com>
2025-04-06 13:37:59 -0700

    Tree Traversals

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -1,6 +1,6 @@
 #ifndef RECCHECK
 //if you want to add any #includes like <iostream> you must do them here (before the next endif)
-
+#include <iostream>
 #endif
 
 #include "equal-paths.h"
@@ -9,10 +9,43 @@ using namespace std;
 
 // You may add any prototypes of helper functions here
 
+// helper function to find height
+int checkHeights(Node* node, int heightCount, int* heightExpec){
+
+    // nothing to check here
+    if (node == nullptr){
+        return 0;
+    }
+
+    // check for leaf
+    if (node->left == nullptr && node->right == nullptr) {
+
+        if (*heightExpec == -1){
+            // set expected
+            *heightExpec = heightCount;
+        }
+
+        // check if current height matches expected
+        return (heightCount== *heightExpec);
+    }
+
+    // recursion, check each substree
+    bool leftTree = checkHeights(node->left, heightCount + 1, heightExpec);
+    bool rightTree = checkHeights(node->right, heightCount + 1, heightExpec);
+    return leftTree && rightTree;
+}
 
 bool equalPaths(Node * root)
 {
     // Add your code below
 
-}
+    // if tree empty
+    if (root == nullptr){
+        // the heights are equal
+        return true;
+    }
 
+    int startingHeight = -1;
+    return checkHeights(root, 0, &startingHeight);
+
+}
